09f2518ed2f969805750278b87090a95ad9a7356
Kai DeLorenzo <git@seaoflaurels.com>
2024-05-29 11:26:52 -0500

    removed hardcoded js file

--- README.md
+++ README.md
@@ -20,8 +20,8 @@
         https://open.spotify.com/album/5koG6JeFEwcINyN1QuXyiq
         https://open.spotify.com/album/3UEEPh5wsdhP7SKC31yvhu
 - [ ]   maybe interleave search results
+- [ ]   add whatever gets stuff to show up on the channels and support tabs on the cannel page. i think it is just polycentric stuff but idk
 - [x]   (i think this is fixed) when the liked songs collection is in the recently played genre it doesn't show up in grayjay
-- [ ]   is this perma? web-player.169ab096
 
 ## Grayjay Bugs
 - [ ]   RatingScaler doesn't work
--- src/SpotifyScript.ts
+++ src/SpotifyScript.ts
@@ -187,13 +187,38 @@ function enable(conf: SourceConfig, settings: Settings, savedState: string | nul
         // the token stored in state might be old
         check_and_update_token()
     } else {
-        const { token_response, user_data } = download_bearer_token()
+        const home_page = "https://open.spotify.com"
+        const token_regex = /<script id="config" data-testid="config" type="application\/json">({.*?})<\/script><script id="session" data-testid="session" type="application\/json">({.*?})<\/script>/
+        const web_player_js_regex = /https:\/\/open\.spotifycdn\.com\/cdn\/build\/web-player\/web-player\..{8}\.js/
+
+        // use the authenticated client to get a logged in bearer token
+        const html = local_http.GET(home_page, {}, true).body
+
+        const web_player_js_match_result = html.match(web_player_js_regex)
+        if (web_player_js_match_result === null || web_player_js_match_result[0] === undefined) {
+            throw new ScriptException("regex error")
+        }
+
+        const token_match_result = html.match(token_regex)
+        if (token_match_result === null || token_match_result[1] === undefined || token_match_result[2] === undefined) {
+            throw new ScriptException("regex error")
+        }
+
+        const user_data: {
+            readonly isPremium: boolean
+            readonly userCountry: string
+        } = JSON.parse(token_match_result[1])
+
+        const token_response: {
+            readonly accessToken: string,
+            readonly accessTokenExpirationTimestampMs: number
+        } = JSON.parse(token_match_result[2])
         const bearer_token = token_response.accessToken
 
         // download license uri and get logged in user
         const get_license_url_url = "https://gue1-spclient.spotify.com/melody/v1/license_url?keysystem=com.widevine.alpha&sdk_name=harmony&sdk_version=4.41.0"
         const profile_attributes_url = "https://api-partner.spotify.com/pathfinder/v1/query?operationName=profileAttributes&variables=%7B%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2253bcb064f6cd18c23f752bc324a791194d20df612d8e1239c735144ab0399ced%22%7D%7D"
-        const web_player_js_url = "https://open.spotifycdn.com/cdn/build/web-player/web-player.169ab096.js"
+        const web_player_js_url = web_player_js_match_result[0]
         const responses = local_http
             .batch()
             .GET(
@@ -224,11 +249,11 @@ function enable(conf: SourceConfig, settings: Settings, savedState: string | nul
         const profile_attributes_response: ProfileAttributesResponse = JSON.parse(
             responses[1].body
         )
-        const match_result = responses[2].body.match(/"(web-player_(.*?))"/)
-        if (match_result === null) {
+        const feature_version_match_result = responses[2].body.match(/"(web-player_(.*?))"/)
+        if (feature_version_match_result === null) {
             throw new ScriptException("regex error")
         }
-        const feature_version = match_result[1]
+        const feature_version = feature_version_match_result[1]
         if (feature_version === undefined) {
             throw new ScriptException("regex error")
         }
@@ -252,29 +277,6 @@ function enable(conf: SourceConfig, settings: Settings, savedState: string | nul
     }
 }
 function download_bearer_token() {
-    if (bridge.isLoggedIn()) {
-        const home_page = "https://open.spotify.com"
-        const regex = /<script id="config" data-testid="config" type="application\/json">({.*?})<\/script><script id="session" data-testid="session" type="application\/json">({.*?})<\/script>/
-
-        // use the authenticated client to get a logged in bearer token
-        const html = local_http.GET(home_page, {}, true).body
-
-        const match_result = html.match(regex)
-        if (match_result === null || match_result[1] === undefined || match_result[2] === undefined) {
-            throw new ScriptException("regex error")
-        }
-
-        const user_data: {
-            readonly isPremium: boolean
-            readonly userCountry: string
-        } = JSON.parse(match_result[1])
-
-        const token_response: {
-            readonly accessToken: string,
-            readonly accessTokenExpirationTimestampMs: number
-        } = JSON.parse(match_result[2])
-        return { token_response, user_data }
-    }
     const get_access_token_url = "https://open.spotify.com/get_access_token?reason=transport&productType=web-player"
 
     // use the authenticated client to get a logged in bearer token
@@ -284,7 +286,7 @@ function download_bearer_token() {
         readonly accessToken: string,
         readonly accessTokenExpirationTimestampMs: number
     } = JSON.parse(access_token_response)
-    return { token_response, user_data: { isPremium: false } }
+    return token_response
 }
 function check_and_update_token() {
     // renew the token with 30 seconds to spare
@@ -292,24 +294,19 @@ function check_and_update_token() {
         return
     }
     log("Spotify log: refreshing bearer token")
-    const { token_response, user_data } = download_bearer_token()
+    const token_response = download_bearer_token()
     let state: State = {
         feature_version: local_state.feature_version,
         bearer_token: token_response.accessToken,
         expiration_timestamp_ms: token_response.accessTokenExpirationTimestampMs,
         license_uri: local_state.license_uri,
-        is_premium: user_data.isPremium
+        is_premium: local_state.is_premium
     }
     if (local_state.username !== undefined) {
-
         state = { ...state, username: local_state.username }
     }
     if (local_state.country !== undefined) {
-
         state = { ...state, country: local_state.country }
-    }
-    if ("userCountry" in user_data) {
-        state = { ...state, country: user_data.userCountry }
     }
     local_state = state
 }
